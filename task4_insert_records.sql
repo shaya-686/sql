fc3071604554937a08eb31c85d7636074c8e9905
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-05-09 19:44:21 +0300

    SQL lesson2

---
+++ task4_insert_records.sql
@@ -0,0 +1,16 @@
+INSERT INTO FRUITS_AND_VEGETABLES (
+	PRODUCT_NAME,
+ 	PRODUCT_TYPE,
+	COLOR, 
+	CALORIC_CONTENT, 
+	DESCRIPTION)
+VALUES ('Apple', 1, 'Red', 52, 'A crunchy and juicy fruit'),
+       ('Banana', 1, 'Yellow', 89, 'A sweet and creamy fruit'),
+       ('Carrot', 2, 'Orange', 41, 'A crunchy and nutritious vegetable'),
+       ('Strawberry', 1, 'Red', 32, 'A small and sweet fruit'),
+       ('Broccoli', 2, 'Green', 34, 'A nutritious green vegetable'),
+       ('Orange', 1, 'Orange', 47, 'A citrus fruit rich in Vitamin C'),
+       ('Spinach', 2, 'Green', 23, 'A leafy green vegetable high in iron'),
+       ('Grapes', 1, 'Purple', 69, 'Small, juicy fruits often used in wine-making'),
+       ('Cucumber', 2, 'Green', 16, 'A refreshing and hydrating vegetable'),
+       ('Peach', 1, 'Orange', 39, 'A soft and fuzzy fruit with a sweet taste');
