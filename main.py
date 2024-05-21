aedb9688c0cc6abf39f3c2dc506bc7b332ff6d5b
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-05-21 19:59:59 +0300

    SQL l5

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-#first commit
+from sqlalchemy import create_engine, Column, Integer, String, Sequence, Date
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.sql import text
+import json
+
+with open("config.json", "r") as f:
+    data = json.load(f)
+    db_user = data['user']
+    db_password = data['password']
+
+db_url = f'postgresql+psycopg2://{db_user}:{db_password}@localhost:5432/schooldb'
+engine = create_engine(db_url)
+
+Base = declarative_base()
+
+
+# create table users
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, Sequence('user_id_sec'), primary_key=True)
+    name = Column(String(20))
+    age = Column(Integer)
+
+
+# add table to db
+Base.metadata.create_all(bind=engine)
+
